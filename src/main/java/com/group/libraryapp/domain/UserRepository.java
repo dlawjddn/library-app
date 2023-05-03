96f54fb57555e1f8e489272098360bde201d6232
dlawjddn <jwl5015@naver.com>
2023-05-04 00:34:21 +0900

    using transaction

--- src/main/java/com/group/libraryapp/domain/UserRepository.java
+++ src/main/java/com/group/libraryapp/domain/UserRepository.java
@@ -3,4 +3,5 @@ package com.group.libraryapp.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByName(String name);
 }
